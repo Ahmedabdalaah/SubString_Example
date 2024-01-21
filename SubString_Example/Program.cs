00dfbbb255c22315827f933d88471c05bb0b39ec
Ahmed <ahmed.ismail14141414@gmail.com>
2024-01-21 18:28:36 +0200

    Add project files.

---
+++ SubString_Example/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SubString_Example;
+using System.Text.RegularExpressions;
+Substringg sub;
+try
+{
+    Console.WriteLine("Enter input here");
+    string input = Console.ReadLine();
+    if(input != "")
+    {
+        Console.WriteLine("Enter first index to substring  input");
+        string f = Console.ReadLine();
+       if(Regex.IsMatch(f, "^[0-9]"))
+        {
+            Console.WriteLine("Enter last index to substring input");
+            string l=Console.ReadLine();
+            if (Regex.IsMatch(f, "^[0-9]"))
+            {
+                int first = Convert.ToInt32(f);
+                int last = Convert.ToInt32(l);
+                sub = new Substringg(input, first, last);
+
+            }
+        }
+       else
+        {
+            Console.WriteLine("Invalid Input");
+        }
+
+    }
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
---
+++ SubString_Example/Substringg.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubString_Example
+{
+    internal class Substringg
+    {
+        public Substringg(string input,int first , int last)
+        {
+            try
+            {
+                string result = "";
+                    result += input.ToString().Substring(first, last);
+                Console.WriteLine(result);
+            }
+           catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
